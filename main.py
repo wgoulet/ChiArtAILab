6ad4fcbe99bace16db8cfe033e987c66d10c5507
Walter Goulet <wgoulet@gmail.com>
2023-08-10 14:16:24 +0000

    Print top 100 most similar results

--- main.py
+++ main.py
@@ -18,10 +18,13 @@ def main(argv):
     omlusr = os.environ.get("OMLUSERNAME")
     omlpass = os.environ.get("OMLPASS")
     oml.connect(user=omlusr,password=omlpass,dsn="aidb_medium",automl="aidb_medium_pool")
-    chiartdata = oml.sync(table='CHIARTDATA').head(20)
+    chiartdata = oml.sync(table='CHIARTDATA').head(500)
     # The column names in the data have to be in uppercase apparently; otherwise the esa model
     # won't load correctly!
-    chiartdata = chiartdata.rename({'description':'DESCRIPTION','artist_name':'ARTIST_NAME','chiartinstid':'CHIARTINSTID'}) 
+    chiartdata = chiartdata.rename({'description':'DESCRIPTION','artist_name':'ARTIST_NAME','chiartinstid':'CHIARTINSTID',
+                                    'title':'TITLE'}) 
+    # drop duplicate descriptions
+    chiartdata = chiartdata.drop_duplicates(subset=['DESCRIPTION'])
     print(chiartdata)
     datadf = chiartdata.pull()
 
@@ -62,13 +65,18 @@ def main(argv):
                             compare_cols = 'DESCRIPTION', 
                             supplemental_cols = ['CHIARTINSTID'])
     print(results)
-    resultdf = results.sort_values(by = ['SIMILARITY'],ascending=False).head(1).pull()
-    sim = resultdf.loc[0]['SIMILARITY']
-    idA = resultdf.loc[0]['CHIARTINSTID_A']
-    idB = resultdf.loc[0]['CHIARTINSTID_B']
-    print(f"With highest correlation {sim} entries with index {idA} and {idB} are most similar.")
-    print(f"Record A: {datadf.loc[datadf.CHIARTINSTID == idA,'DESCRIPTION'].values[0]}")
-    print(f"Record B: {datadf.loc[datadf.CHIARTINSTID == idB,'DESCRIPTION'].values[0]}")
+    resultdf = results.sort_values(by = ['SIMILARITY'],ascending=False).head(100).pull()
+    for i in resultdf.index:
+        sim = resultdf.loc[i]['SIMILARITY']
+        idA = resultdf.loc[i]['CHIARTINSTID_A']
+        idB = resultdf.loc[i]['CHIARTINSTID_B']
+        print(f"High correlation {sim} entries with index {idA} and {idB} are similar.")
+        print(f"Record A: {datadf.loc[datadf.CHIARTINSTID == idA,'DESCRIPTION'].values[0]}")
+        print(f"Record A: {datadf.loc[datadf.CHIARTINSTID == idA,'ARTIST_NAME'].values[0]}")
+        print(f"Record A: {datadf.loc[datadf.CHIARTINSTID == idA,'TITLE'].values[0]}")
+        print(f"Record B: {datadf.loc[datadf.CHIARTINSTID == idB,'DESCRIPTION'].values[0]}")
+        print(f"Record B: {datadf.loc[datadf.CHIARTINSTID == idB,'ARTIST_NAME'].values[0]}")
+        print(f"Record B: {datadf.loc[datadf.CHIARTINSTID == idB,'TITLE'].values[0]}")
 
 
 if __name__ == "__main__":
